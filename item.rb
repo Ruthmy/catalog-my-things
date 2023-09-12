745850bce67047c5b7d99d4fa63f84708c665e31
Ruthmy <ruthmy17@gmail.com>
2023-09-12 16:09:59 -0600

    Modified 'item.rb' -file- - Modified the constructor method to just two parameters. - Add personalized setter methods for 'genre', 'author' and 'label'. - Delete 'source' arguments and methods, they don't aply for the project

--- item.rb
+++ item.rb
@@ -1,25 +1,35 @@
 class Item
-  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_accessor :id, :publish_date, :archived
 
-  def initialize(genre, author, source, label, publish_date)
-    self.class.id_counter = (self.class.id_counter || 0) + 1
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
+  def initialize(publish_date, id = nil)
+    @id = id || Random.rand(1..10_000)
+    @genre = []
+    @author = []
+    @label = []
     @publish_date = publish_date
-    private
-    @id = self.class.id_counter
     @archived = false
   end
 
+  # Setter methods for 1-to-many relationships
+  def genre=(genre)
+    @genre = genre
+    genre.add_item(self) unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.add_item(self) unless author.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.add_item(self) unless label.items.include?(self)
+  end
+
+  # Methods
   def move_to_archive
     @archived = true if can_be_archived?
   end
-
-  private
-
-  attr_reader :id, :archived
 
   def can_be_archived?
     return true if Time.now.year - @publish_date.year > 10
