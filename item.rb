75843f565a261d6c7741567943b7a245e451a1e7
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-12 17:11:28 -0500

    Change the id to a random number and mofidied the can_be_archived method

--- item.rb
+++ item.rb
@@ -2,14 +2,12 @@ class Item
   attr_accessor :genre, :author, :source, :label, :publish_date
 
   def initialize(genre, author, source, label, publish_date)
-    self.class.id_counter = (self.class.id_counter || 0) + 1
     @genre = genre
     @author = author
     @source = source
     @label = label
     @publish_date = publish_date
-    private
-    @id = self.class.id_counter
+    @id = Random.rand(1..1000)
     @archived = false
   end
 
@@ -17,12 +15,12 @@ class Item
     @archived = true if can_be_archived?
   end
 
+  attr_reader :id, :archived
+  
   private
 
-  attr_reader :id, :archived
-
   def can_be_archived?
-    return true if Time.now.year - @publish_date.year > 10
+    return true if Time.now.year - @publish_date.to_i > 10
 
     false
   end
