3234ba283abf9c3a9c4cd1f3337196ee86f1c71f
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-12 10:02:01 -0500

    Add the super method in the Game constructor

--- game.rb
+++ game.rb
@@ -4,6 +4,7 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played_at)
+    super()
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
