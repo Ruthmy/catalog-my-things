d67cc9fefb9fb42eb42d2c31e651ed7c04642a67
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-12 17:10:07 -0500

    Add the parameters in the constructor and modified the can_be_archived method

--- game.rb
+++ game.rb
@@ -3,14 +3,14 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer, last_played_at)
+  def initialize(genre, author, source, label, publish_date, multiplayer, last_played_at)
     super(genre, author, source, label, publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
   def can_be_archived?
-    return true if item.can_be_archived? && Time.now.year - @last_played_at > 2
+    return true if super && Time.now.year - @last_played_at.to_i > 2
 
     false
   end
