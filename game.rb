f0d5f84457a1d881b585b442390175f130ffe73b
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-12 15:53:39 -0500

    Add the parameters of the super item constructor un Game class

--- game.rb
+++ game.rb
@@ -4,7 +4,7 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played_at)
-    super()
+    super(genre, author, source, label, publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
