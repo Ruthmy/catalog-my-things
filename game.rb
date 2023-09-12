ae32f0c5bbb9eca3b2d35d946e2652a2259cf6d8
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-12 09:30:10 -0500

    Require the Item class and create a can_be_archived method in Game class

--- game.rb
+++ game.rb
@@ -1,6 +1,18 @@
-class Game
+require_relative 'item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
   def initialize(multiplayer, last_played_at)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    if item.can_be_archived? && Time.now.year - @last_played_at.year > 2
+      return true
+    else
+      return false
+    end
   end
 end
