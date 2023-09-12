dd8a8eca3ce98696d842d73422e382848b585fb2
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-12 15:52:19 -0500

    Create methods to create and read the information from the json files

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ class App
     create_data
     @books = []
     @albums = []
-    @games = []
-    @authors = []
     @labels = []
     @genres = []
+    create_file_if_not_exists('./data/games.json')
+    create_file_if_not_exists('./data/authors.json')
   end
 
   # Creates a data directory if not exists
@@ -18,6 +18,30 @@ class App
     return if Dir.exist?('./data')
 
     Dir.mkdir('./data')
+  end
+
+  # Sets the arrays to be empty or to be the parsed info from the files
+  def load_data_from_files
+    @games = load_data_from_files('./data/games.json', [])
+    @authors = load_data_from_files('./data/authors.json', [])
+  end
+
+  # A method to check if the files are empty or not, and parse the info
+  def load_json_file(file_path, default_value)
+    file = File.open(file_path)
+    file_data = file.read
+    if file_data.empty?
+      default_value
+    else
+      JSON.parse(file_data)
+    end
+  end
+
+  # Creates the json files if they don't exist
+  def create_file_if_not_exists(file_path)
+    return if File.exist?(file_path)
+
+    File.open(file_path, 'w')
   end
 
   def option_select
