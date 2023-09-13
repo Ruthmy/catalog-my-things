d52ba6e48633ab3a44bf2e06db4df651857792af
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-13 10:09:00 -0500

    Revert "Modifyt the parameters in the Game constructor to take a hash as a parameter"
    This reverts commit 2775ac16ff775ec0dc38eea9c7b169511dbd605d.

--- game.rb
+++ game.rb
@@ -3,10 +3,10 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(options = {})
-    super(options[:genre], options[:author], options[:label], options[:publish_date])
-    @multiplayer = options[:multiplayer]
-    @last_played_at = options[:last_played_at]
+  def initialize(genre, author, label, publish_date, multiplayer, last_played_at)
+    super(genre, author, label, publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
 
   def can_be_archived?
