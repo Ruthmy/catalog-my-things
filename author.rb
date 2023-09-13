5d7be7ca7a121d017edfefb5b30fc48dfdd103bd
Alexandre <alexsales001@hotmail.com>
2023-09-13 19:54:08 -0300

    conflict fixes

--- author.rb
+++ author.rb
@@ -1,4 +1,3 @@
-require_relative 'item'
 class Author
   attr_accessor :first_name, :last_name
 
--- game.rb
+++ game.rb
@@ -1,4 +1,3 @@
-require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
