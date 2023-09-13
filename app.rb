c5041b133024e829f50565074f20177ae5e6869d
Ruthmy <ruthmy17@gmail.com>
2023-09-13 14:54:56 -0600

    Modified 'app.rb' -file- - Created the lis methods list_genres, list_labels. - Created the add_genre, select_genre, select_label and add_music_album

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
+require_relative 'music_album'
+require_relative 'genre'
+
 class App
   def initialize
     @books = []
@@ -48,11 +51,24 @@ class App
   end
 
   def list_genres
-    puts 'this will list the genres'
+    if @genres.empty?
+      puts '---> There are no genres, please add one first'
+    else
+      @genres.each_with_index do |genre, index|
+        puts "#{index} - #{genre.name}"
+      end
+      puts ''
+    end
   end
 
   def list_labels
-    puts 'this will list the labels'
+    if @labels.empty?
+      puts 'There are no labels, please add one'
+    else
+      @labels.each do |label|
+        puts " > #{label.name}"
+      end
+    end
   end
 
   def list_authors
@@ -63,8 +79,68 @@ class App
     puts 'this will add a book'
   end
 
+  def add_genre
+    print "\n* Creating a new genre *\n\nPlease enter the name of the new genre:  "
+    genre_name = gets.chomp
+    genre = Genre.new(genre_name)
+    @genres << genre
+    puts "\n---> Genre '#{genre_name}' added\n"
+  end
+
+  def select_genre
+    puts "\n* Please enter the genre number from the list below\n\n"
+    list_genres
+    puts "\n* If genre doesn't exist, type 'new' to create a new one\n"
+    genre_name = gets.chomp
+    if genre_name == 'new'
+      add_genre
+      puts "\n* Thanks for adding a new genre. Now, please enter the genre number from the list below\n\n"
+      list_genres
+      genre_name = gets.chomp
+      @genres[genre_name.to_i]
+    elsif genre_name.to_i < @genres.length
+      @genres[genre_name.to_i]
+    else
+      return puts "\n---> Genre does not exist, you will be redirected to the main menu\n\n"
+    end
+  end
+
+  def select_label
+    puts "\n* Please enter the label number from the list below\n\n"
+    list_labels
+    puts "\n* If label doesn't exist, type 'new' to create a new one, otherwise you'll be redirected to the main menu\n"
+    label_name = gets.chomp
+    if label_name == 'new'
+      add_label
+      puts "\n* Thanks for adding a new label. Now, please enter the label number from the list below\n\n"
+      list_labels
+      label_name = gets.chomp
+      label = @labels[label_name.to_i]
+    elsif label_name.to_i < @labels.length
+      label = @labels[label_name.to_i]
+    else
+      puts "\n---> Label does not exist, you will be redirected to the main menu\n\n"
+    end
+  end
+
   def add_music_album
-    puts 'this will add a music album'
+    print '* Please anter the publish date: '
+    publish_date = gets.chomp
+    # Process to select/add a genre
+    genre = select_genre
+    # Process to select/add an author
+    # author = select_author
+    # Process to select/add a label
+    # label = select_label
+    puts 'Is it on spotify? (y/n)'
+    on_spotify = gets.chomp.upcase == 'Y'
+    # Process to add a new music album
+    music_album = MusicAlbum.new(publish_date, on_spotify: on_spotify)
+    music_album.genre = genre
+    @albums << music_album
+    print "@albums #{@albums}\n"
+    # music_album.author = author
+    # music_album.label = label
   end
 
   def add_game
