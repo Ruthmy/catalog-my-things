fe970e60a93c7a8374f2275786571da1c0678e34
Manuel Alejandro Sánchez Sierra <manuelsanchez059@gmail.com>
2023-09-14 11:21:50 -0500

    Move the @items array into the constructor and modify the conditional inside the add_item method

--- author.rb
+++ author.rb
@@ -3,9 +3,9 @@ require_relative 'item'
 class Author
   attr_accessor :first_name, :last_name
 
-  @items = []
-
+  
   def initialize(first_name, last_name)
+    @items = []
     @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
@@ -15,7 +15,9 @@ class Author
 
   # has-many relationship with Item
   def add_item(item)
-    @items << item if item.is_a?(Item)
-    item.author = self
+    if item.is_a?(Item)
+      @items << item
+      item.author = self
+    end
   end
 end
