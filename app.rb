bb95b7b40468d98df712d0499ca7a05b5567c8de
Ruthmy <ruthmy17@gmail.com>
2023-09-15 07:25:56 -0600

    Modified 'app.rb' -file- - Added 'add_music_album' method to get the user input. - Added 'create_object_music_album' to create the MusicAlbum instance. - Added 'create_music_album' to create the necessary instances and json.

--- app.rb
+++ app.rb
@@ -136,7 +136,54 @@ class App
   end
 
   def add_music_album
-    puts 'this will add a music album'
+    print 'Genre: '
+    genre = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    print 'Label: '
+    label = gets.chomp
+    print 'Publish date [Day/Month/Year]: '
+    publish_date = gets.chomp
+    print 'Is it on spotify? (y/n)'
+    on_spotify = gets.chomp.upcase == 'Y'
+    create_music_album({
+                         genre:,
+                         author:,
+                         label:,
+                         publish_date:,
+                         on_spotify:
+                       })
+  end
+
+  def create_music_album(options)
+    names = options[:author].split # Split the name at the spaces.
+    first_name = names[0]
+    last_name = names[1] if names.length > 1
+    add_author_if_doesnt_exist(first_name, last_name)
+    add_label(options[:label])
+    add_genre_if_doesnt_exist(options[:genre])
+
+    music_album = create_object_music_album(options)
+    music_album_input = {
+      'id' => music_album.id,
+      'author' => options[:author],
+      'genre' => music_album.genre,
+      'label' => music_album.label,
+      'on_spotify' => music_album.on_spotify,
+      'archived' => music_album.can_be_archived?
+    }
+    @music_albums << music_album_input
+    File.write('./data/music_albums.json', JSON.pretty_generate(@music_albums))
+  end
+
+  def create_object_music_album(options)
+    MusicAlbum.new(
+      options[:genre],
+      options[:author],
+      options[:label],
+      options[:publish_date],
+      on_spotify: options[:on_spotify]
+    )
   end
 
   def add_label(label_name)
